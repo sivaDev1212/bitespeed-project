a3a00115bf65190f6161d8ea9fc4d1a4e5c8be7f
sivaDev1212 <connectwithsivaprasad@gmail.com>
2025-06-08 13:11:20 +0530

    porst lisiting

--- src/index.js
+++ src/index.js
@@ -144,5 +144,6 @@ app.get('/fulldata', async (req, res) => {
     client.release();
   }
 });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
